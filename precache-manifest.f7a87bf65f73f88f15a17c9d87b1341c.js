7fb4e8305a79c940b30d5a8f5b4f6baaf669a4d3
hiMinju <minju980508@gmail.com>
2020-10-01 22:40:27 +0900

    Updates

---
+++ precache-manifest.f7a87bf65f73f88f15a17c9d87b1341c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c696c852eed22f22d13e7aed7b8d9ff",
+    "url": "/Bistroad_Web/index.html"
+  },
+  {
+    "revision": "c57d45e7c1fed4cdfe5c",
+    "url": "/Bistroad_Web/static/css/main.3dd4b1f9.chunk.css"
+  },
+  {
+    "revision": "ed77aac78ac141b8693a",
+    "url": "/Bistroad_Web/static/js/2.57091186.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/Bistroad_Web/static/js/2.57091186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57d45e7c1fed4cdfe5c",
+    "url": "/Bistroad_Web/static/js/main.f03bf6de.chunk.js"
+  },
+  {
+    "revision": "31bb9aa5e0dbfa7f8758",
+    "url": "/Bistroad_Web/static/js/runtime-main.9a78545b.js"
+  }
+]);
